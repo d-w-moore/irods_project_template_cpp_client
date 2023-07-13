202c6d2b67bae1cd1c46b1a829328ee0053f201e
d-w-moore <dmoore@renci.org>
2023-07-13 08:47:31 -0400

    got  segmentation fault on iRODS 4.3.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,25 @@
+#include <iostream>
+#include <irods/lsUtil.h> // For load_client_api_plugins()
 #include <irods/rodsClient.h> // For load_client_api_plugins()
 #include <irods/client_connection.hpp>
 #include <irods/filesystem.hpp>
 #include <irods/irods_exception.hpp>
 
 #include <fmt/format.h>
+#include <irods/irods_query.hpp>
 
 int main(int _argc, char* _argv[]) // NOLINT(modernize-use-trailing-return-type)
 {
+    (void) _argc;
+    (void) _argv;
+    /*
 	if (_argc != 2) {
 		// NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
 		fmt::print("usage: {} LOGICAL_PATH\n", _argv[0]);
 		return 1;
 	}
+
+        */
 
 	load_client_api_plugins();
 
@@ -20,10 +28,28 @@ int main(int _argc, char* _argv[]) // NOLINT(modernize-use-trailing-return-type)
 	try {
 		irods::experimental::client_connection conn;
 
+                RcComm* connPtr {conn};
+
+                rodsEnv client_environment;
+                auto status = getRodsEnv(&client_environment);
+                if (status < 0) {
+                    std::cerr << "bad environment" << std::endl;
+                    exit(1);
+                }
+
+                char MyTicket[]{"myticket"};
+                setSessionTicket(connPtr, MyTicket);
+                irods::query<RcComm> q{connPtr,"select COLL_NAME,DATA_NAME"};
+                for (const auto & i:q) {
+                    std::cout << i[0] <<'/'<<i[1]<< std::endl;
+                }
+
+                /**
 		// NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
 		for (const auto& e : fs::client::collection_iterator{conn, _argv[1]}) {
 			fmt::print("{}\n", e.path().c_str());
 		}
+                **/
 
 		return 0;
 	}
